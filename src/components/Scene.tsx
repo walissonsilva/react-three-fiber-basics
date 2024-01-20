028d6d4373bdd661597b90a7b41e3b8e59f5ddba
Walisson Silva <walissonsilva.dev@gmail.com>
2024-01-19 23:04:49 -0300

    feat: add avatar to scene

--- src/components/Scene.tsx
+++ src/components/Scene.tsx
@@ -1,10 +1,24 @@
+import { OrbitControls } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
 import { Avatar } from "./Avatar";
 
-export const Scene = () => {
+export const Scene: React.FC = () => {
   return (
-    <Canvas>
-      <Avatar />
+    <Canvas shadows camera={{ position: [0, 2, 5], fov: 30 }}>
+      <SceneContent />
     </Canvas>
+  );
+};
+
+export const SceneContent = () => {
+  return (
+    <>
+      <OrbitControls />
+      <ambientLight intensity={1.5} />
+
+      <group position={[0, -1, 0]}>
+        <Avatar />
+      </group>
+    </>
   );
 };
